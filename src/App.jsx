f4e97e44456d6354329507990337a0d58010d1d3
Dhanunjaya-Elluri <dhanunjayet@gmail.com>
2024-12-17 16:54:07 +0100

    fix: Updated reroute to profile-cv

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import './App.css';
 function App() {
   return (
     <ThemeProvider>
-      <Router>
+      <Router basename="/profile-cv">
         <div className="app">
           <Navbar />
           <main className="main-content">
