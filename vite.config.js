66a35a4b9889e6f0f56ae9f52f6e4e8f983223dd
Dhanunjaya Elluri <dhanunjayet@gmail.com>
2024-12-20 09:07:13 +0530

    fix: fixing build warnings in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom"],
+          firebase: ["firebase/app", "firebase/database"],
+        },
+      },
+    },
+  },
 });
